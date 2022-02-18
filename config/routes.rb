aa8cf88936086e74e46591150ec0428867f9d084
Jackie <jackie@Jackies-MacBook-Pro.local>
2022-02-18 17:44:53 +0700

    demo

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # devise_for :users,
+  #             path: '',
+  #             path_names: {sign_in: 'login' ,sign_out: 'logout' ,edit: 'profile',sign_up: 'resgistration'},
+  #             controllers: {omniauth_callbacks: 'omniauth_callbacks' }
+
+  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
